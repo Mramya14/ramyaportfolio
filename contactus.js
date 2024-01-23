5bbd6d1a5e49ff65efc536235beb8a16fd3cb19e
Mramya14 <121632166+Mramya14@users.noreply.github.com>
2024-01-23 14:58:28 +0530

    Add files via upload

---
+++ contactus.js
@@ -0,0 +1,24 @@
+function sendmail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_swdk5er";
+    const templateID = "template_y641bag";
+
+    emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("your message sent successfully");
+    })
+    .catch((err) => console.log(err));
+}
+   
